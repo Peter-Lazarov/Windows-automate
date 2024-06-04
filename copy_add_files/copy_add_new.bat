a60bb14740c0e97204e36ba8eb260de6f9114aeb
Peter Lazarov <peter_lazarov@hotmail.com>
2024-06-04 23:34:46 +0300

    Create copy_add_new.bat
    This command can be used for copying files from large folder without remove them first. Only add them or overwrite them.

---
+++ copy_add_files/copy_add_new.bat
@@ -0,0 +1,3 @@
+xcopy "D:\Downloads\Firebird database\zaplati\data*" "D:\Downloads\Firebird database\flash\" /e /d /y
+xcopy "D:\Downloads\Firebird database\zaplati\Archiv" "D:\Downloads\Firebird database\flash\Archiv\" /e /d /y
+pause
