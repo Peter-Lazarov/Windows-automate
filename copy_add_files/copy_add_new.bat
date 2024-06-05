d2ff11088b39d6f65abc164feae8f8a7229c72b5
Peter Lazarov <peter_lazarov@hotmail.com>
2024-06-05 10:02:52 +0300

    Update copy_add_new.bat

--- copy_add_files/copy_add_new.bat
+++ copy_add_files/copy_add_new.bat
@@ -1,3 +1,4 @@
 xcopy "D:\Downloads\Firebird database\zaplati\data*" "D:\Downloads\Firebird database\flash\" /e /d /y
-xcopy "D:\Downloads\Firebird database\zaplati\Archiv" "D:\Downloads\Firebird database\flash\Archiv\" /e /d /y
+@REM xcopy "D:\Downloads\Firebird database\zaplati\Archiv" "D:\Downloads\Firebird database\flash\Archiv\" /d:01-01-2024 /e /y
+xcopy "D:\Downloads\Firebird database\zaplati\Archiv" "D:\Downloads\Firebird database\flash\Archiv\" /d /e /y
 pause
